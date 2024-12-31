3429dc0ae7bdfd40b9d6b5bc6abe68d9277a42e2
Nich Secord <github@secord.io>
2024-12-31 13:12:04 -0800

    feat(nix): Extract common MacOS configuration to base module

--- hosts/business/v1.nix
+++ hosts/business/v1.nix
@@ -1,105 +1,31 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  self,
+  ...
+}:
+{
+  imports = [
+    "${self}/modules/darwin/base.nix"
+  ];
+
   nixpkgs.hostPlatform = "aarch64-darwin";
-  nixpkgs.config.allowUnfree = true;
+  system = {
+    stateVersion = 5;
+  };
 
   nixpkgs.config.permittedInsecurePackages = [
     "dotnet-sdk-7.0.410"
   ];
   environment.systemPackages = [
-    pkgs.mkalias
-
-    pkgs.alacritty
-    pkgs.neovim
-    pkgs.nixfmt-rfc-style
-    pkgs.tmux
-    pkgs.ripgrep
-    pkgs.fzf
-    pkgs.zoxide
-    pkgs.stow
-    pkgs.oh-my-posh
-
-    pkgs.obsidian
-
-    pkgs.nodejs_22
-    pkgs.python3
-    ppkgs.dotnet-sdk_7
-
-    # TODO: Investigate why these packages aren't working.
-    # pkgs.raycast
-    # pkgs.vscode
-    # pkgs.lazygit
+    pkgs.dotnet-sdk_7
   ];
 
   homebrew = {
     enable = true;
     brews = [
-      "nvm"
-      "lazygit"
-
       # Languages
       "mysql"
     ];
-    casks = [
-      "todoist"
-      "raycast"
-      "visual-studio-code"
-      "font-meslo-lg-nerd-font"
-    ];
-    onActivation = {
-      cleanup = "zap";
-      autoUpdate = true;
-      upgrade = true;
-    };
-  };
-
-  # MacOS System Configuration
-  system = {
-    stateVersion = 5;
-    configurationRevision = self.rev or self.dirtyRev or null;
-
-    activationScripts = {
-      applications = {
-        text =
-          let
-            env = pkgs.buildEnv {
-              name = "system-applications";
-              paths = config.environment.systemPackages;
-              pathsToLink = "/Applications";
-            };
-          in
-          pkgs.lib.mkForce ''
-            # Set up applications.
-            echo "setting up /Applications..." >&2
-            rm -rf /Applications/Nix\ Apps
-            mkdir -p /Applications/Nix\ Apps
-            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-            while read -r src; do
-              app_name=$(basename "$src")
-              echo "copying $src" >&2
-              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-            done
-          '';
-      };
-    };
-
-    defaults = {
-      dock = {
-        autohide = true;
-        persistent-apps = [
-          "${pkgs.alacritty}/Applications/Alacritty.app"
-          "${pkgs.obsidian}/Applications/Obsidian.app"
-          "/Applications/Todoist.app"
-        ];
-      };
-      finder = {
-        FXPreferredViewStyle = "clmv";
-      };
-      NSGlobalDomain = {
-        AppleICUForce24HourTime = true;
-        AppleInterfaceStyle = "Dark";
-        KeyRepeat = 2;
-      };
-    };
   };
 }

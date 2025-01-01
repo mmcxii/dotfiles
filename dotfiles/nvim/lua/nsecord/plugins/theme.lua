ad9ba8ff67eb57de905e8b6ebf222edda628f5aa
Nich Secord <github@secord.io>
2025-01-01 12:15:57 -0800

    feat(ghostty,nvim): Update theme to rose-pine

--- dotfiles/nvim/lua/nsecord/plugins/theme.lua
+++ dotfiles/nvim/lua/nsecord/plugins/theme.lua
@@ -1,13 +1,22 @@
 return {
   {
-    "oxfist/night-owl.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
-      require("night-owl").setup()
-      vim.cmd.colorscheme("night-owl")
-      vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+      vim.cmd.colorscheme("rose-pine")
     end,
   },
+  --   {
+  --     "oxfist/night-owl.nvim",
+  --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --     priority = 1000, -- make sure to load this before all the other start plugins
+  --     config = function()
+  --       -- load the colorscheme here
+  --       require("night-owl").setup()
+  --       vim.cmd.colorscheme("night-owl")
+  --       vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+  --     end,
+  --   },
 }
